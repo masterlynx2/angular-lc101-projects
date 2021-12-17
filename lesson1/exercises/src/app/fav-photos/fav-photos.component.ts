aa56aad0a4aa1fc294b743f75d992e63628c0f09
Masterlynx2 <Tonywatkins325@gmail.com>
2021-12-17 17:33:12 -0500

    why did i turn this in late?

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image1 = 'https://site-cdn.givemesport.com/images/21/12/06/1619217055e44d77d13a90496d52c54c/960.jpg'
+  image2 = 'https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg';
+  image3 = 'https://s3.amazonaws.com/prod-media.gameinformer.com/styles/full/s3/2020/09/29/9fc5dc69/605027-genshinimpact.jpg';
 
   constructor() { }
 
